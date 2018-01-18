e9c86db183c026fdff4f193798551a150f39ad64
rafaelg5 <rafaelg5@ciencias.unam.mx>
2018-01-18 01:56:31 -0600

    tablas de tipos y símbolos

--- include/sym_table.h
+++ include/sym_table.h
@@ -1,28 +1,48 @@
 #ifndef SYM_TABLE_H
 #define SYM_TABLE_H
 
+#include "type_table.h"
+
+/*
+* Estructura para representar un símbolo.
+* id es el identificador del símbolo.
+* type es el tipo del símbolo.
+* dir es la dirección en memoria del símbolo.
+* params son los parámetros en caso de que el símbolo sea una función
+* next es el siguiente símbolo en la lista, para evitar colisiones en el
+* diccionario.
+*/
 typedef struct symbol {
   char* id;
   int type;
-  int scope;
-  struct symbol* next; // Next symbol in bucket
+  int dir;
+  List* params;
+  struct symbol* next;
 } Symbol;
 
-#ifndef HT_SIZE
-  #define HT_SIZE 1024
-#endif
+// Tamaño del diccionario
+#define HT_SIZE 1024
 
+/*
+* Estructura para la tabla de símbolos.
+* table es un arreglo de símbolos que representa el diccionario (hash table) con
+* los pares key = id del símbolo y valor = símbolo.
+* parent es la tabla padre, ya que al crear un struct o una función se define
+* un nuevo alcance para las variables.
+* count es el número de símbolos en la tabla.
+*/
 typedef struct sym_table {
   Symbol* table[HT_SIZE];
+  struct sym_table* parent;
   int count;
+  Type_Table* type_table;
 } Sym_Table;
 
-// Declaración de la tabla de símbolos
-Sym_Table* st;
-
+Sym_Table* init_sym_table();
 Symbol* new_symbol(char*, int, int);
 int hash();
-Symbol* lookup(char*);
-Symbol* insert(char*, int, int);
+Symbol* lookup(Sym_Table*, char*);
+Symbol* insert(Sym_Table*, char*, int, int);
+void set_symbol_params(Symbol*, List*);
 
 #endif
--- src/sym_table.c
+++ src/sym_table.c
@@ -1,40 +1,68 @@
 #include <stdlib.h>
 #include <string.h>
 #include "../include/sym_table.h"
+#include "../include/type_table.h"
 
-void init_table(){
+/**
+* Inicializa los atributos de una tabla de símbolos.
+* @return un apuntador a la tabla de símbolos
+*/
+Sym_Table* init_sym_table(){
 
-    st = (Sym_Table*) malloc(sizeof(Sym_Table));
+    Sym_Table* st = (Sym_Table*) malloc(sizeof(Sym_Table));
 
     for(int i = 0; i < HT_SIZE; i++)
       st->table[i] = NULL;
 
     st->count = 0;
+    st->parent = NULL;
+    st-> type_table = init_type_table();
+
+    return st;
 }
 
-Symbol* new_symbol(char* name, int type, int scope){
+/**
+* Crea un nuevo símbolo.
+* @param name el nombre o identificador del símbolo
+* @param type el tipo del símbolo
+* @param dir la dirección en memoria del símbolo
+* @return un apuntador al símbolo
+*/
+Symbol* new_symbol(char* name, int type, int dir){
 
   Symbol* sym = (Symbol*) malloc(sizeof(Symbol));
   sym->id = name;
   sym->type = type;
-  sym->scope = scope;
+  sym->dir = dir;
+  sym->params = NULL;
   sym->next = NULL;
   return sym;
 }
 
-int hash(char* s){
+/**
+* Función hash para el diccionario.
+* @param s la llave
+* @return un entero entre 0 y HT_SIZE - 1
+*/
+int hash(char* key){
 
     int hash_val = 0;
 
-    while(*s != '\0'){
-      hash_val = (hash_val << 4) + *s;
-       s++;
+    while(*key != '\0'){
+      hash_val = (hash_val << 4) + *key;
+       key++;
     }
 
     return hash_val;
 }
 
-Symbol* lookup(char* name){
+/**
+* Busca un símbolo en la tabla.
+* @param st la tabla en la que se quiere buscar
+* @param name el nombre o id del símbolo
+* @return un apuntador al símbolo, si existe, e.o.c devuelve NULL
+*/
+Symbol* lookup(Sym_Table* st, char* name){
 
   int h = hash(name);
   int index = h % HT_SIZE;
@@ -50,13 +78,30 @@ Symbol* lookup(char* name){
   return sym;
 }
 
-Symbol* insert(char* name, int type, int scope){
+/**
+* Inserta un nuevo símbolo en la tabla.
+* @param st la tabla en la que se quiere buscar
+* @param name el nombre o id del símbolo
+* @param type el tipo del símbolo
+* @param dir la dirección en memoria del símbolo
+* @return un apuntador al nuevo símbolo
+*/
+Symbol* insert(Sym_Table* st, char* name, int type, int dir){
 
   int h = hash(name);
   int index = h % HT_SIZE;
-  Symbol* sym = new_symbol(name, type, scope);
+  Symbol* sym = new_symbol(name, type, dir);
   sym->next = st->table[index];
   st->table[index] = sym;
   st->count++;
   return sym;
+}
+
+/**
+* Agrega la lista de parámetros/argumentos de una función
+* @param sym el símbolo
+* @param params la lista de parámetros
+*/
+void set_symbol_params(Symbol* sym, List* params){
+  sym->params = params;
 }
